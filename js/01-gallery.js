32333ebc588f676a41d71f6e376987899852e712
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-10-14 18:47:06 +0300

    1 task-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const mainGallery = document.querySelector(".gallery");
+const createdImages = galleryItemsCreate(galleryItems);
+mainGallery.insertAdjacentHTML('beforeend', createdImages);
+mainGallery.addEventListener("click", onClick);
 
+
+function onClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return
+    } 
+}
+
+
+
+
+function galleryItemsCreate(galleryItems) {
+    return galleryItems.map(
+        ({ description, preview, original }) =>
+            `<a class="gallery__link" href="${original}">
+            <img class="gallery__image"
+            src="${preview}"
+            alt="${description}"/></a>
+            `).join('');
+}
 console.log(galleryItems);
