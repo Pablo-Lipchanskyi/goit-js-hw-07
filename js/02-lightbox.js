0f0cebcc7c4c9a6a3f7d8ef45cabfeabb96779b0
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-10-15 13:28:56 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const mainGallery = document.querySelector(".gallery");
+const createdImages = galleryItemsCreate(galleryItems);
+mainGallery.insertAdjacentHTML('beforeend', createdImages);
+mainGallery.addEventListener("click", onClick);
 
+
+function onClick(event) {
+    event.preventDefault();
+    const instance = basicLightbox.create(`<div class="modal"><img src="${event.target.dataset.source}" width="800" height="800"></div>`)
+    if (event.target.nodeName !== "IMG") {
+        return
+    } 
+    instance.show();
+}
+
+
+
+
+function galleryItemsCreate(galleryItems) {
+    return [...galleryItems].map(
+        ({ description, preview, original }) =>
+            `<a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+            src="${preview}"
+            data-source = "${original}"
+            alt="${description}"/></a>
+            `).join('');
+}
 console.log(galleryItems);
